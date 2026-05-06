1dd70494927a6256366184e341dca7b7c66486f8
Junha Yoo <junha.yoo@mail.mcgill.ca>
2026-05-06 15:19:45 -0400

    [MINOR] resolve path conflict during import

--- vpr_models/apgem.py
+++ vpr_models/apgem.py
@@ -10,9 +10,12 @@ import gdown
 import sklearn
 import torch
 
-sys.path.append(str(Path("third_party/deep-image-retrieval")))
+# sys.path.append(str(__path__))
+script_path = Path(__file__).parent.parent
+sys.path.append(
+    str(script_path.joinpath("third_party/deep-image-retrieval").expanduser().resolve())
+)
 os.environ["DB_ROOT"] = ""  # required by dirtorch
-
 from dirtorch.extract_features import load_model  # noqa: E402
 from dirtorch.utils import common  # noqa: E402
 
@@ -45,7 +48,9 @@ class GeM(torch.nn.Module):
         dir_models = {
             "Resnet-101-AP-GeM": "https://docs.google.com/uc?export=download&id=1UWJGDuHtzaQdFhSMojoYVQjmCXhIwVvy",
         }
-        checkpoint = Path(torch.hub.get_dir(), "dirtorch", self.conf["model_name"] + ".pt")
+        checkpoint = Path(
+            torch.hub.get_dir(), "dirtorch", self.conf["model_name"] + ".pt"
+        )
         if not checkpoint.exists():
             checkpoint.parent.mkdir(exist_ok=True, parents=True)
             link = dir_models[self.conf["model_name"]]
@@ -72,7 +77,9 @@ class GeM(torch.nn.Module):
                 # at a time
                 desc = desc.unsqueeze(0)  # batch dimension
                 pca = self.net.pca[self.conf["whiten_name"]]
-                desc = common.whiten_features(desc.cpu().numpy(), pca, **self.conf["whiten_params"])
+                desc = common.whiten_features(
+                    desc.cpu().numpy(), pca, **self.conf["whiten_params"]
+                )
                 desc = torch.from_numpy(desc)
                 assert len(desc) == 1
                 desc = desc[0]
